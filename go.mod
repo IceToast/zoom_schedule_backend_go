0bb15110195b9e51858db88ebd259072d4528563
Julian Goeritz <julian.goeritz@otto.de>
2021-03-20 22:59:07 +0100

    Implemented beta OAuth2 functionality by using the goth_fiber library and adding a sample Google auth provider

--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module zoom_schedule_backend_go
 go 1.16
 
 require (
-	github.com/gofiber/fiber v1.14.6 // indirect
-	github.com/gofiber/fiber/v2 v2.6.0 // indirect
-	github.com/subosito/gotenv v1.2.0 // indirect
-	go.mongodb.org/mongo-driver v1.5.0 // indirect
+	github.com/gofiber/fiber/v2 v2.6.0
+	github.com/markbates/goth v1.67.1
+	github.com/shareed2k/goth_fiber v0.2.1
+	github.com/subosito/gotenv v1.2.0
+	go.mongodb.org/mongo-driver v1.5.0
 )
--- main.go
+++ main.go
@@ -1,26 +1,81 @@
 package main
 
 import (
+	// System-Imports
+	"log"
+	// eigene Imports
 	"zoom_schedule_backend_go/routes"
 
+	// GitHub Imports
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/providers/google"
+	"github.com/shareed2k/goth_fiber"
 	"github.com/subosito/gotenv"
 )
-
-const port = ":8011"
+const (
+	Port         = ":8011"
+	GoogleKey    = "googlekey" // via Google API
+	GoogleSecret = "googlesec" // via Google API
+)
 
 func init() {
 	gotenv.Load()
 }
 
 func main() {
-
 	app := fiber.New()
+	
+	goth.UseProviders(
+		google.New(GoogleKey, GoogleSecret,
+			"http://localhost" + Port + "/api/auth/google/callback"),
+		// ... mehr Provider
+	)
+	
 	app.Use(cors.New())
-	app.Get("/meeting/:id?", routes.GetMeeting)
-	app.Post("/meeting", routes.CreateMeeting)
-	app.Put("/meeting/:id", routes.UpdateMeeting)
-	app.Delete("/meeting/:id", routes.DeleteMeeting)
-	app.Listen(port)
+	
+	// OAuth2-Endpunkte
+	api := app.Group("/api") // /api
+	api.Get("/auth/:provider", func(ctx *fiber.Ctx) error {
+		if gothUser, err := goth_fiber.CompleteUserAuth(ctx); err == nil {
+			ctx.JSON(gothUser)
+		} else {
+			goth_fiber.BeginAuthHandler(ctx)
+		}
+		return nil
+	})
+	api.Get("/auth/:provider/callback", func(ctx *fiber.Ctx) error {
+		user, err := goth_fiber.CompleteUserAuth(ctx)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		ctx.JSON(user)
+		return nil
+	})
+	api.Get("/auth/logout/:provider", func(ctx *fiber.Ctx) error {
+		if err := goth_fiber.Logout(ctx); err != nil {
+			log.Fatal(err)
+		}
+
+		ctx.Redirect("/")
+		return nil
+	})
+
+	// Meeting-Endpunkte
+	api.Get("/meeting/:id?", routes.GetMeeting)
+	api.Post("/meeting", routes.CreateMeeting)
+	api.Put("/meeting/:id", routes.UpdateMeeting)
+	api.Delete("/meeting/:id", routes.DeleteMeeting)
+
+	// Test-Endpunkt
+	api.Get("/test", func(ctx *fiber.Ctx) error {
+		ctx.Format("<p><a href='/api/auth/google'>Google Auth</a></p>")
+		return nil
+	})
+
+	if err := app.Listen(Port); err != nil {
+		log.Fatal(err)
+	}
   }
