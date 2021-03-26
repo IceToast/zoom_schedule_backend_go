64486717e66462084bf0f7230583a9f817e6e89b
IceToast <socia@icetoast.de>
2021-03-26 19:10:31 +0100

    Added Day Struct

--- routes/userRoute.go
+++ routes/userRoute.go
@@ -15,7 +15,7 @@ type User struct {
 	_id      primitive.ObjectID `json:"id,omitempty" bson:"id,omitempty"`
 	UserName     string             `json:"name,omitempty" bson":name.omitempty"`
 	Email     string             `json:"link,omitempty" bson":link.omitempty`
-	 string             `json:"password,omitempty" bson:"password,omitempty"`
+	Days []Day             `json:"days,omitempty" bson:"days,omitempty"`
 }
 
 type Meeting struct {
@@ -27,6 +27,8 @@ type Meeting struct {
 
 type Day struct {
 	_id primitive.ObjectID `json:"id,omitempty" bson:"id,omitempty"`
+	Name string `json:"name,omitempty" bson:"name.omitempty"`
+	Meetings []Meeting `json:"meetings,omitempty" bson:"meetings.omitempty"`
 }
 
 const dbName = "zoom_schedule"
